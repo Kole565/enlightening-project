b4d5766dba52f8c4bf9c188873276441c8c766ae
Kole565 <rjkz565rjkz565@gmail.com>
2023-12-26 16:40:32 +0300

    Start Lobby dev

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour {
+
+    private new Camera camera;
+
+    [SerializeField] private float zoom;
+
+    void Start () {
+        camera = GetComponent<Camera>();
+    }
+
+    void Update () {
+        camera.orthographicSize = zoom;
+    }
+}
